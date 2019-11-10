4a41f510d155df0a43a1ab1c679ad7cf4faeb1a5
Zam1995 <57448418+Zam1995@users.noreply.github.com>
2019-11-10 10:24:41 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ class VocalTrainer(MycroftSkill):
     @intent_file_handler('trainer.vocal.intent')
     def handle_trainer_vocal(self, message):
         self.speak_dialog('trainer.vocal')
+        
+    @intent_file_handler('male.vocal.intent')
+    def handle_male_vocal(self, message):
+        self.speak_dialog('male.vocal')
 
 
 def create_skill():
